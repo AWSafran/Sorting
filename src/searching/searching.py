5ef032a3d143b187fda62217e91926a860a140e4
AWSafran <andrewsafran@gmail.com>
2019-07-16 16:50:06 -0400

    linear search and recursive binary search implemented

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -2,22 +2,24 @@
 def linear_search(arr, target):
   
   # TO-DO: add missing code
-
-   return -1   # not found
+  for i in range(0, len(arr)):
+    if arr[i] == target:
+      return i
+  return -1
 
 
 # STRETCH: write an iterative implementation of Binary Search 
-def binary_search(arr, target):
+# def binary_search(arr, target):
 
-  if len(arr) == 0:
-    return -1 # array empty
+#   if len(arr) == 0:
+#     return -1 # array empty
     
-  low = 0
-  high = len(arr)-1
+#   low = 0
+#   high = len(arr)-1
 
-  # TO-DO: add missing code
+#   # TO-DO: add missing code
 
-  return -1 # not found
+#   return -1 # not found
 
 
 # STRETCH: write a recursive implementation of Binary Search 
@@ -27,4 +29,12 @@ def binary_search_recursive(arr, target, low, high):
 
   if len(arr) == 0:
     return -1 # array empty
+
+  if arr[middle] == target:
+    return middle
+  
+  if arr[middle] > target:
+    return binary_search_recursive(arr, target, low, middle)
+  else:
+    return binary_search_recursive(arr, target, middle, high)
   # TO-DO: add missing if/else statements, recursive calls
